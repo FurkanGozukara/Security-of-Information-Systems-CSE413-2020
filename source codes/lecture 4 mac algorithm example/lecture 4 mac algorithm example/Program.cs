bf51e1a6200dc85430ca675395e90c57b5d7a2a9
Furkan Gözükara <furkangozukara@gmail.com>
2020-11-02 01:52:15 +0300

    lecture 4

---
+++ source codes/lecture 4 mac algorithm example/lecture 4 mac algorithm example/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static lecture_4_mac_algorithm_example.hmac;
+using System.IO;
+
+namespace lecture_4_mac_algorithm_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string srKey = File.ReadAllText("key.txt");
+            string srMac = computeMacValue(File.ReadAllText("some long text.txt"), srKey);
+            receiver(File.ReadAllText("some long text.txt"), srMac);
+            Console.Read();
+        }
+
+        static void receiver(string srMsg, string srMac)
+        {
+            string srKey = File.ReadAllText("key.txt");
+            string srMacReCalculate= computeMacValue(srMsg, srKey);
+            if(srMacReCalculate== srMac)
+            {
+                Console.WriteLine("received message was authentic and not forged");
+                Console.WriteLine("received mac value: "+ srMac);
+                Console.WriteLine("new computed mac value: " + srMacReCalculate);
+            }
+        }
+    }
+}
---
+++ source codes/lecture 4 mac algorithm example/lecture 4 mac algorithm example/hmac.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace lecture_4_mac_algorithm_example
+{
+    public static class hmac
+    {
+        static string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256   
+            
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        public static string computeMacValue(string srInput, string srKey)
+        {
+            //since the collision chance of SHA256 is close to 0, this should pretty well work
+            return ComputeSha256Hash(srKey + srInput);
+        }
+
+    }
+}
