ed56bb08564b3a105850a7c14ed029feead3b592
Furkan Gözükara <furkangozukara@gmail.com>
2020-12-07 23:45:25 +0300

    lecture 8 slides and source code

---
+++ source codes/lecture 8 - salted password hashing/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace lecture_8___salted_password_hashing
+{
+    class Program
+    {
+     
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your password to register");
+            string srUserRawPassword = Console.ReadLine();
+
+            string srHashedPw = srUserRawPassword.returnHashedPw();
+
+            File.WriteAllText("saltedpw.txt",srHashedPw);
+
+            while(true)
+            {
+
+            Console.WriteLine("enter your password to login");
+
+            string srLoginPw =  Console.ReadLine();
+
+                Console.WriteLine("your entered password is: " + srLoginPw);
+
+                Console.WriteLine("the salted hash of your password is " + srLoginPw.returnHashedPw());
+
+                string srSavedHashedPw = File.ReadAllText("saltedpw.txt");
+
+                Console.WriteLine("saved password hash is " + srSavedHashedPw);
+
+                if(srSavedHashedPw== srLoginPw.returnHashedPw())
+                {
+                    Console.WriteLine("login success both hashes match");
+                }
+                else
+                    Console.WriteLine("login fail wrong password");
+
+
+                Console.WriteLine("click a key to continue");
+
+                Console.ReadKey();
+            }
+
+        }
+
+     
+    }
+}
---
+++ source codes/lecture 8 - salted password hashing/genericExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace lecture_8___salted_password_hashing
+{
+   public static class genericExtensions
+    {
+        private static readonly string srSalt = File.ReadAllText("password_salt.txt");
+        public static string ComputeSha256Hash(this string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        public static string returnHashedPw(this string srRawPassword)
+        {
+            return ComputeSha256Hash(srSalt + srRawPassword);
+        }
+    }
+}
